d23311455ce71f0d90986922068ef85219a2b575
Dani Heras <danielherascabrera@gmail.com>
2018-05-02 23:36:25 +0200

    New styles for courses list

--- src/app/_views/Cursos/Cursos.js
+++ src/app/_views/Cursos/Cursos.js
@@ -5,8 +5,21 @@ import axios from "axios/index";
 import {$api_URL} from "../../config/constants";
 import {Col, Row} from "reactstrap";
 import CourseCard from "../../_components/course_card/CourseCard";
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import Grid from 'material-ui/Grid';
 import {FaBook} from 'react-icons/lib/fa';
-import {TextField} from "material-ui";
+import {
+    Avatar,
+    IconButton,
+    ListItem,
+    ListItemAvatar,
+    ListItemSecondaryAction,
+    ListItemText,
+    TextField
+} from "material-ui";
+import List from "material-ui/es/List/List";
+import Card from "../../_components/card/card";
+import Link from "react-router-dom/es/Link";
 
 class Cursos extends Component {
 
@@ -45,37 +58,63 @@ class Cursos extends Component {
     render() {
         return (
             <div className={'fade-in'}>
-                <div className={'title'}>
-                    <h3><span><FaBook/></span> My<span className={'bold'}>Courses</span></h3>
-                    {
-                        (this.state.courses.length !== 0) &&
-                        <TextField
-                            className={'__search'}
-                            name={'search'}
-                            label="Search course"
-                            margin="normal"
-                            onChange={this.handleSearch}
-                        />
-                    }
-                </div>
-
-                {
-                    (!this.state.courses.length) && <div>You do not belong to any course</div>
-                }
-
                 <Row>
-                    {
-                        this.state.search.map((course, key) => (
-                            <Col xs={12} sm={6} md={4} lg={3} key={key}>
-                                <CourseCard course={course}/>
-                            </Col>
-                        ))
-                    }
+                    <Col xs={12} sm={12} md={6}>
+                        <Card style={{padding: '2rem'}}>
+                            {
+                                (!this.state.courses.length) && <div>You do not belong to any course</div>
+                            }
+
+
+                            <Row>
+                                <Col xs={12}>
+                                    <div className={'title'} style={{textAlign: 'center'}}>
+                                        <h3><span><FaBook/></span> My<span className={'bold'}>Courses</span></h3>
+                                    </div>
+                                </Col>
+                                <Col xs={12} className={'__search'}>
+                                    {
+                                        (this.state.courses.length !== 0) &&
+                                        <TextField
+                                            className={'__search'}
+                                            name={'search'}
+                                            label="Search"
+                                            margin="normal"
+                                            onChange={this.handleSearch}
+                                        />
+                                    }
+                                </Col>
+                            </Row>
+                            <List>
+                                {this.state.search.map((course, key) => (
+                                        <ListItem key={key}>
+                                            <ListItemAvatar>
+                                                <Avatar
+                                                    src={course.picture + course.id}/>
+                                            </ListItemAvatar>
+                                            <ListItemText primary={course.name} secondary={course.category}/>
+                                            <ListItemSecondaryAction>
+                                                <Link to={'/app/course/'+course.id}>
+                                                    <IconButton>
+                                                        <VisibilityIcon/>
+                                                    </IconButton>
+                                                </Link>
+                                            </ListItemSecondaryAction>
+                                        </ListItem>
+                                    )
+                                )}
+                            </List>
+
+
+                        </Card>
+                    </Col>
                 </Row>
+
 
             </div>
         );
     }
 };
 
-export default Cursos; 
+export default Cursos;
+
