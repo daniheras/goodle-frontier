4ae32be2799a3d6ac3599a549a9db75498b45358
Dani Heras <danielheras@bewanted.com>
2018-05-21 16:47:12 +0200

    axios config in process

--- src/app/_views/Courses/Courses.js
+++ src/app/_views/Courses/Courses.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 
-import CourseList from '../../_components/courseList/courseList';
+//import CourseList from '../../_components/courseList/courseList';
 import './Courses.scss';
+import axios from '../../config/axios';
+
+import styled from 'styled-components';
+
+const CourseView = styled.div`
+    
+`;
 
 class Courses extends Component {
+
+    componentWillMount() {
+        axios.get('/courses');
+    }
     
     render(){
         return (
-            <div>
-                Works
-            </div>
+            <CourseView>
+                <h2>
+                    Courses<span className={'bold'}>List</span>
+                </h2>
+            </CourseView>
         )
     }
 
