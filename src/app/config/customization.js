e1473c9435293302b5a875f15d9a4309ac0daf37
Dani Heras <danielherascabrera@gmail.com>
2018-06-02 19:55:36 +0200

    Added support to jpg background and Created customization file
    The new customization file contains at this moment an object called themes that wrapps the names of all hosted backgrounds and an array of recommended colors for each image that the user can pick from.

---
+++ src/app/config/customization.js
@@ -0,0 +1,81 @@
+export const themes = [
+  {
+    theme: 'forest',
+    recomendedColors: [
+      "rgba(55,202,118,.58)",
+      "rgba(39,186,156,.58)"
+    ]
+  },
+  {
+    theme: 'city',
+    recomendedColors: [
+      "rgba(57,150,215,.58)",
+      "rgba(153,90,177,.58)",
+      "rgba(55,74,93,.58)"
+    ]
+  },
+  {
+    theme: 'fantasy',
+    recomendedColors: [
+      "rgba(229,79,67,.58)",
+      "rgba(55,202,118,.58)",
+      "rgba(57,150,215,.58)"
+    ]
+  },
+  {
+    theme: 'firewatch',
+    recomendedColors: [
+      "rgba(228,126,53,.58)",
+      "rgba(209,86,31,.58)",
+      "rgba(240,195,58,.58)",
+      "rgba(242,155,51,.58)",
+      "rgba(229,79,67,.58)",
+    ]
+  },
+  {
+    theme: 'forest',
+    recomendedColors: [
+      "rgba(55,202,118,.58)",
+      "rgba(57,150,215,.58)",
+      "rgba(39,186,156,.58)"
+    ]
+  },
+  {
+    theme: 'mountains',
+    recomendedColors: [
+      "rgba(228,126,53,.58)",
+      "rgba(209,86,31,.58)",
+      "rgba(240,195,58,.58)",
+      "rgba(242,155,51,.58)",
+      "rgba(229,79,67,.58)",
+      "rgba(57,150,215,.58)",
+      "rgba(153,90,177,.58)",
+    ]
+  },
+  {
+    theme: 'winter',
+    recomendedColors: [
+      "rgba(57,150,215,.58)",
+      "rgba(153,90,177,.58)",
+      "rgba(55,74,93,.58)",
+      "rgba(55,202,118,.58)",
+      "rgba(39,186,156,.58)"
+    ]
+  },
+  {
+    theme: 'wolf',
+    recomendedColors: [
+      "rgba(153,90,177,.58)",
+      "rgba(55,74,93,.58)",
+      "rgba(229,79,67,.58)",
+    ]
+  },
+  {
+    theme: 'asia',
+    recomendedColors: [
+      "rgba(55,202,118,.58)",
+      "rgba(57,150,215,.58)",
+      "rgba(39,186,156,.58)"
+    ]
+  }
+]
